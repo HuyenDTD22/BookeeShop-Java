6928a54febedf0bfd8b5868176b6c1163ab136d5
HuyenDang <huyendtb22dccn400@gmail.com>
2026-03-14 20:45:36 +0700

    refactor(category): add lombok annotation for CategoryTreeResponse

--- backend/bookeeshop/src/main/java/com/huyen/bookeeshop/dto/response/CategoryTreeResponse.java
+++ backend/bookeeshop/src/main/java/com/huyen/bookeeshop/dto/response/CategoryTreeResponse.java
@@ -1,8 +1,16 @@
 package com.huyen.bookeeshop.dto.response;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
 import java.util.List;
 import java.util.UUID;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class CategoryTreeResponse {
     UUID id;
 
