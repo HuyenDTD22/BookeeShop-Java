4d459687292cf6c079d9e0725b73aff5c23be98b
HuyenDang <huyendtb22dccn400@gmail.com>
2026-03-25 09:37:30 +0700

    feat: remove email validation from username field in AuthenticationRequest

--- backend/bookeeshop/src/main/java/com/huyen/bookeeshop/dto/request/AuthenticationRequest.java
+++ backend/bookeeshop/src/main/java/com/huyen/bookeeshop/dto/request/AuthenticationRequest.java
@@ -1,6 +1,5 @@
 package com.huyen.bookeeshop.dto.request;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
@@ -12,7 +11,6 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class AuthenticationRequest {
     @NotBlank
-    @Email(message = "USERNAME_INVALID")
     String username;
 
     @NotBlank
